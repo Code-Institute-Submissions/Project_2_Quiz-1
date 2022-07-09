c30d25335050a6a1e7529ed96de3f56cc0b2b9ac
PayG1 <micah.athe.009@gmail.com>
2022-07-09 17:41:02 +0000

    created array for upcoming question and defined points system

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,3 +8,24 @@ let Answers = true
 let score = 0
 let availableQuestion = []
 
+let questions = [
+    {
+        question:'ques1',
+        option1: '1',
+        option2: '2',
+        option3: '3',
+        option4: '4',
+        Answer: '3'
+    },
+]
+
+const Score_Points = 100
+const Max_Questions = 10
+
+startGame = () => {
+    questionConter = 0
+    score = 0
+    availableQuestions = [...questions] 
+    getNewQuestions{}
+}
+
