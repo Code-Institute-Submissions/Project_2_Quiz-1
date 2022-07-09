8664f2ce87b104a466d3f4d942145371426b7163
PayG1 <micah.athe.009@gmail.com>
2022-07-09 20:08:25 +0000

    created endScreen.html and js files, added basic html structure to enscreen

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,22 +10,13 @@ let availableQuestions = []
 
 let questions = [
     {
-        question:'What is the capital of Finland?',
-        option1: 'Paris',
-        option2: 'London',
-        option3: 'Helsinki',
-        option4: 'Stockholm',
-
-        answer: 'Helsinki'
-    },
-    {
         question:'Which five colours make up the Olympic rings?',
         option1: 'blue,yellow,black,green,red',
         option2: 'red,orange,pink,white,purple',
         option3: 'red,red,red,red,red',
         option4: 'black,green,orange,purple,grey',
 
-        answer: 'blue,yellow,black,green,red'
+        answer: 1,
     },
     {
         question:'Who painted the Mona Lisa?',
@@ -34,16 +25,16 @@ let questions = [
         option3: 'Charles Darwin',
         option4: 'Leonardo da Vinci',
 
-        answer: 'Leonardo da Vinci'
+        answer: 4,
     },
     {
-        question:'Which planet is closest to the sun?',
+        question:'Which Planet is closest to the sun?',
         option1: 'Earth',
         option2: 'Jupiter',
         option3: 'Moon',
         option4: 'Mercury',
 
-        answer: 'Mercury'
+        answer: 4,
     },
     {
         question:'How many elements are there in the periodic table?',
@@ -52,34 +43,34 @@ let questions = [
         option3: '52',
         option4: '99',
 
-        answer: '118'
+        answer: 1,
+    },
+    {
+        question:'How many valves does the heart have?',
+        option1: '4',
+        option2: '12',
+        option3: '5',
+        option4: '9',
+
+        answer: 1,
     },
     {
         question:'Which country\'s rugby team is known as The Springboks?',
-        option1: 'Paris',
-        option2: 'London',
-        option3: 'Helsinki',
-        option4: 'Stockholm',
-
-        answer: 'Helsinki'
-    },
-    {
-        question:'What is the capital of Finland?',
         option1: 'India',
         option2: 'USA',
         option3: 'South Africa\'s',
         option4: 'Australia',
 
-        answer: 'South Africa\'s'
+        answer: 3,
     },
     {
         question: 'What is the smallest country in the world?',
         option1: 'Japan',
-        option2: 'Canada',
+        option2: 'Vatican City',
         option3: 'Argentina',
         option4: 'Spain',
 
-        answer: 'Vatican City'
+        answer: 2,
     },
     {
         question:'What was the most streamed show on Netflix in 2020',
@@ -88,16 +79,25 @@ let questions = [
         option3: 'Squid Game',
         option4: 'Demon Slayer',
 
-        answer: 'The Umbrella Academy season 2'
+        answer: 1,
     },
     {
         question:'In what franchise would you find the character Katniss Everdeen?',
         option1: 'The Maze Runner',
         option2: 'Game of Thrones',
-        option3: 'The hunger Games',
+        option3: 'The Hunger Games',
         option4: 'The Cycle',
 
-        answer: 'The hunger Games'
+        answer: 3,
+    },
+    {
+        question:'What is the capital of Finland?',
+        option1: 'Paris',
+        option2: 'London',
+        option3: 'Helsinki',
+        option4: 'Stockholm',
+
+        answer: 3,
     }
 
 ]
@@ -109,23 +109,23 @@ startGame = () => {
     questionCounter = 0
     score = 0
     availableQuestions = [...questions] 
-    getNewQuestions()
+    getNewQuestion()
 }
 
 getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > Max_Questions) {
         localStorage.setItem('newestScore', score)
 
-        return window.location.assign('/end.html')
+        return window.location.assign('/endScreen.html')
     }
 
 const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
 currentQuestion = availableQuestions[questionsIndex]
 question.innerText = currentQuestion.question
 
-choices.forEach(choice => {
-    const number = choice.dataset['number']
-    choice.innerText = currentQuestion['choice' + number]
+choices.forEach(option => {
+    const number = option.dataset['number']
+    option.innerText = currentQuestion['option' + number]
 })
 
 availableQuestions.splice(questionsIndex, 1)
@@ -133,23 +133,23 @@ availableQuestions.splice(questionsIndex, 1)
 Answers = true 
 }
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
+choices.forEach(option => {
+    option.addEventListener('click', e => {
         if(!Answers) return
 
         Answers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
+        const selectedOption = e.target
+        const selectedAnswer = selectedOption.dataset['number']
 
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
         if(classToApply === 'correct') {
             incrementScore(Score_Points)
         }
-        selectedChoice.parentElement.classList.add(classToApply)
+        selectedOption.parentElement.classList.add(classToApply)
 
         setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
+            selectedOption.parentElement.classList.remove(classToApply)
             getNewQuestion()
         }, 100)
     })
